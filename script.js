a1bbe226115c2a276f439b7a43c64d7cba5e3ff5
Asaf31214 <asaf.kanlipicak@outlook.com>
2023-01-19 08:14:18 +0300

    sss design and program html

--- script.js
+++ script.js
@@ -9,13 +9,16 @@ hamburgerCloseBtn.addEventListener('click', () => {
 })
 
 document.body.addEventListener('click', (event) => {
+
     const cardsList=event.target.parentElement.parentElement.children;
-    for(card of cardsList){
-        if(card.lastElementChild.classList.contains("answer-open") && card.lastElementChild!=event.target.parentElement.lastElementChild){
-            card.lastElementChild.classList.toggle("answer-open")
+        for(card of cardsList){
+            if(card.lastElementChild.classList.contains("answer-open") && card.lastElementChild!=event.target.parentElement.lastElementChild){
+                card.lastElementChild.classList.toggle("answer-open")
+            }
         }
-    }
-    if (event.target.parentElement.classList.contains('faq-card')) {
-        event.target.parentElement.lastElementChild.classList.toggle("answer-open")
-    }
+        if (event.target.parentElement.classList.contains('faq-card')) {
+            event.target.parentElement.lastElementChild.classList.toggle("answer-open")
+        }
+    
+    
 });
